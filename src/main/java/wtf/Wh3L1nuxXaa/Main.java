4795e90b5e8541139fb6bc3e415fa4f43dbc4928
Wh3L1nuxXaa <132058006+Wh3L1nuxXaa@users.noreply.github.com>
2024-07-28 14:46:26 +0300

    Create Main.java

---
+++ src/main/java/wtf/Wh3L1nuxXaa/Main.java
@@ -0,0 +1,25 @@
+package wtf.Wh3L1nuxXaa;
+
+import wtf.Wh3L1nuxXaa.library.HTTPConnection;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Enter your auth token");
+        Scanner sc = new Scanner(System.in);
+        String token = sc.nextLine();
+        System.out.println("Enter your max energy");
+        int maxEnerg = sc.nextInt();
+        System.out.println("Enter your tap count");
+        int tapcount = sc.nextInt();
+        System.out.println("Enter cooldown (ms)");
+        int time = sc.nextInt();
+        while (true) {
+            long unixTime = System.currentTimeMillis() / 1000L;
+            System.out.println("[Click] Sended response: " + HTTPConnection.sendRequest(maxEnerg, tapcount, unixTime, token));
+            Thread.sleep(time);
+        }
+    }
+}
